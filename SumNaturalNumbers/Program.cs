1d6b61b8d6fa04c16b563acea102fc9f372c354e
Lasherrie Lathan <101126658+lasherrie@users.noreply.github.com>
2026-01-20 12:11:42 -0600

    Add files via upload

---
+++ SumNaturalNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SumNaturalNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            Console.WriteLine("The first 10 natural number is :");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i + " ");
+                sum = sum + i;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The Sum is : " + sum);
+
+        }
+    }
+}
